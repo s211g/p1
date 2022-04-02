a2fd540b94491a672b9522a485e6e61cfa34ec64
Ser <mail@mail.com>
2022-04-02 08:51:42 +0300

    02_0851

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 #include <memory>
 
-class MyHeap {
+template <typename T> class X {
   public:
-    MyHeap(int i_) {
-        i = i_;
-        std::cout << "MyHeap()" << std::endl;
-    }
-    void f() { std::cout << "i: " << i << std::endl; }
-    int i;
+    void f() { static_cast<T *>(this)->f(); }
 };
 
-void *operator new(size_t size, MyHeap *heap, const char *text) {
-    heap->f();
-    std::cout << "textttt: " << text << std::endl;
-    std::cout << "operator new() size: " << size << std::endl;
-    return new int();
-}
+class A : public X<A> {
+  public:
+    void f() { std::cout << "hi" << std::endl; }
+};
 
 int main() {
-
-    MyHeap heap(123);
-    std::unique_ptr<int>(new (&heap, "meow") int(0));
-    std::cout << "hi" << std::endl;
+    A a;
+    a.f();
     return 0;
 }
