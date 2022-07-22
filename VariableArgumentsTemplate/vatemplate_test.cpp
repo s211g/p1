7a6be279c586170cf650290c217a5c1264789df3
Ser <mail@mail.com>
2022-07-22 13:24:39 +0300

    123

--- VariableArgumentsTemplate/vatemplate_test.cpp
+++ VariableArgumentsTemplate/vatemplate_test.cpp
@@ -9,4 +9,47 @@
 
 namespace vatemplate_test {
 
+    void test_sum17() {
+        std::cout << "test sum c++17" << std::endl;
+        std::cout << "= " << sum17(1, 3, 5) << std::endl;
+    }
+
+    void test_sum14() {
+        std::cout << "test sum c++14" << std::endl;
+        auto s = sum(1, 3, 5);
+        std::cout << "= " << s << std::endl;
+    }
+
+    void test_tgroup() {
+        std::cout << "test types group" << std::endl;
+
+        int ii  = 11;
+        long ll = 22;
+        Group<int, long> g0(ii, std::move(ll));
+        std::cout << std::endl;
+
+        auto g00 = makeGroup(10, 2.2, std::string("cde"));
+        std::cout << std::endl;
+
+        Group<int, long, std::string> g1(1, 2, "abc");
+        std::cout << std::endl;
+
+        std::string s = static_cast<std::string>(g1);
+        int i         = static_cast<int>(g1);
+        long l        = static_cast<long>(g1);
+        std::cout << "s: " << s << " int: " << i << " long: " << l << std::endl
+                  << std::endl;
+
+        const std::string& s1 = static_cast<const std::string&>(g1);
+        const int& i1         = static_cast<const int&>(g1);
+        const long& l1        = static_cast<const long&>(g1);
+        std::cout << "s: " << s1 << " int: " << i1 << " long: " << l1 << std::endl
+                  << std::endl;
+
+        const std::string& s2(g1);
+        const int& i2(g1);
+        const long& l2(g1);
+        std::cout << "s: " << s2 << " int: " << i2 << " long: " << l2 << std::endl
+                  << std::endl;
+    }
 }
--- VariableArgumentsTemplate/vatemplate_test.hpp
+++ VariableArgumentsTemplate/vatemplate_test.hpp
@@ -4,7 +4,88 @@
 
 namespace vatemplate_test {
 
+    void test_sum17();
+
+    template <typename... T>
+    auto sum17(const T&... x) {
+        return (x + ...);
+    }
+
+    void test_sum14();
+
+    template <typename T1>
+    auto sum(const T1& x1) {
+        std::cout << "sum(const T1& x1) x1 = " << x1 << std::endl;
+        return x1;
+    }
+
+    template <typename T1, typename... T>
+    auto sum(const T1& x1, const T&... x) {
+        std::cout << "sum(const T1& x1, const T&... x) x1 = " << x1 << std::endl;
+        return x1 + sum(x...);
+    }
+
+    void test_tgroup();
+
+    template <typename... T>
+    struct Group;
+
+    template <typename T1>
+    struct Group<T1> {
+        T1 t1_;
+        Group() = default;
+        explicit Group(const T1& t1) :
+            t1_(t1) {
+            std::cout << "Group<T1>::Group(const T1& t1) t1 = " << t1_ << std::endl;
+        }
+        explicit Group(T1&& t1) :
+            t1_(std::move(t1)) {
+            std::cout << "Group<T1>::Group(const T1&& t1) t1 = " << t1_ << std::endl;
+        }
+        explicit operator const T1&() const {
+            std::cout << "Group<T1>::operator const T1&() t1 = " << t1_ << std::endl;
+            return t1_;
+        }
+        explicit operator T1&() {
+            std::cout << "Group<T1>::operator T1&() t1 = " << t1_ << std::endl;
+            return t1_;
+        }
+    };
+
+    template <typename T1, typename... T>
+    struct Group<T1, T...> : Group<T...> {
+        T1 t1_;
+        Group() = default;
+        explicit Group(const T1& t1, T&&... t) :
+            Group<T...>(std::forward<T>(t)...), t1_(t1) {
+            std::cout << "Group<T1, T...>::Group(const T1& t1,  T&&... t) t1 = " << t1_ << std::endl;
+        }
+        explicit Group(T1&& t1, T&&... t) :
+            Group<T...>(std::forward<T>(t)...), t1_(std::move(t1)) {
+            std::cout << "Group<T1, T...>::Group(const T1&& t1,  T&&... t) t1 = " << t1_ << std::endl;
+        }
+        explicit operator const T1&() const {
+            std::cout << "Group<T1, T...>::operator const T1&() t1 = " << t1_ << std::endl;
+            return t1_;
+        }
+        explicit operator T1&() {
+            std::cout << "Group<T1, T...>::operator T1&() t1 = " << t1_ << std::endl;
+            return t1_;
+        }
+    };
+
+
+    template <typename... T>
+    auto makeGroup(T&&... t) {
+        return Group<T...>(std::forward<T>(t)...);
+    }
+
+
     inline void test() {
         std::cout << "variable arguments of template tests" << std::endl;
+
+        //test_sum17();
+        //test_sum14();
+        test_tgroup();
     }
 }
