37188d4b9ae6afd5dc843069dbcdf76193620cb9
Ser <mail@mail.com>
2022-09-06 11:05:36 +0300

    1

--- Other/functional_hdr_test.cpp
+++ Other/functional_hdr_test.cpp
@@ -1,6 +1,7 @@
 
 #include "functional_hdr_test.hpp"
 #include <functional>
+#include <string>
 
 namespace functional_hdr_test {
 
@@ -39,6 +40,30 @@ namespace functional_hdr_test {
             std::cout << " result i = " << i << std::endl;
         }
 
+        template <typename... Args>
+        void add_many(Args... args) {
+            int res = i + (args + ...);
+            std::cout << "template add_many(Args... args) i = " << i << ", arguments: ";
+            std::cout << ((std::to_string(args) + " ") + ...);
+            //            ((std::cout << args << " "), ...);
+            std::cout << ", result = " << res << std::endl;
+        }
+
+        template <typename... Args>
+        void literal_progress(Args... args) {
+            std::cout << "template literal_progress(Args... args), arguments: ";
+            std::cout << ((std::to_string(args) + " ") + ...);
+            std::cout << std::endl;
+
+            std::cout << "result: ";
+            std::string v;
+            (((v += std::to_string(args)), std::cout << v << " "), ...);
+            std::cout << std::endl
+                      << "alternative cout)): " << std::endl;
+            ((std::cout << (v += std::to_string(args)) << " "), ...);
+            std::cout << "." << std::endl;
+        }
+
         void operator()(int j) const { std::cout << "operator()(int j) " << j << std::endl; }
         void operator()() const { std::cout << "operator()() " << std::endl; }
     };
@@ -71,6 +96,10 @@ namespace functional_hdr_test {
         std::function<int&(A_function&)> f6 = &A_function::i;
         f6(af)                              = 10;
         std::cout << "f6(af) = 10; af.i = " << af.i << std::endl;
+        // !!! from smart pointer
+        auto sp                                              = std::make_unique<A_function>(123);
+        std::function<int(std::unique_ptr<A_function>&)> f66 = &A_function::i;
+        std::cout << "smart pointer A_function::i = " << f66(sp) << std::endl;
 
         // store a call to a function object
         std::function<void()> f7 = af;
@@ -86,5 +115,27 @@ namespace functional_hdr_test {
         factorial_old = [&factorial_old](int n) { return (n < 2) ? 1 : n * factorial_old(n - 1); };
         std::cout << "factorial_old(3)= " << factorial_old(3) << std::endl;
     }
+
+    void test_memfn() {
+        std::cout << "test std::mem_fn " << std::endl;
+
+        A_function a(0);
+        auto f1 = std::mem_fn(&A_function::add);
+        f1(a, 2);
+
+        // data access
+        auto p = std::make_unique<A_function>(3);
+        auto i = std::mem_fn(&A_function::i);
+        std::cout << "object a.i = " << i(a) << std::endl;
+        std::cout << "start pointer -> i = " << i(p) << std::endl;
+
+        // template function
+        auto f2 = std::mem_fn(&A_function::add_many<int, double>);
+        f2(a, 1, 3.3);
+
+        auto f3 = std::mem_fn(&A_function::literal_progress<int, int>);
+        f3(a, 1, 2);
+    }
+
 
 }
--- Other/functional_hdr_test.hpp
+++ Other/functional_hdr_test.hpp
@@ -6,11 +6,13 @@ namespace functional_hdr_test {
 
     void test_bind();
     void test_function();
+    void test_memfn();
 
     inline void test() {
         std::cout << "functional header tests" << std::endl;
 
         test_bind();
         test_function();
+        test_memfn();
     }
 }
