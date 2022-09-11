7c34de59778f993396d39e6ac25a8dcd6a725c10
Ser <mail@mail.com>
2022-09-11 10:06:26 +0300

    1

--- Other/atomic_hdr_test.cpp
+++ Other/atomic_hdr_test.cpp
@@ -1,6 +1,7 @@
 #include "atomic_hdr_test.hpp"
 #include <atomic>
 #include <string>
+#include <cassert>
 
 namespace atomic_hdr_test {
 
@@ -61,5 +62,70 @@ namespace atomic_hdr_test {
         // += -= &= |= ^=  return new value
         std::cout << "*api+=1 : " << *(api += 1) << ", *api : " << *api << std::endl;
         std::cout << "*api-=1 : " << *(api -= 1) << ", *api : " << *api << std::endl;
+
+        std::cout << "\ntest 2" << std::endl;
+        // std::memory_order_relaxed не гарантирует тот же порядок изменений переменных в другом потоке
+        // нет гарантий что значение измененной переменной сразу же будут видны в другом потоке
+        std::atomic<bool> ab21{false};
+        std::atomic<bool> ab22{false};
+        // thread1
+        {
+            ab21.store(true, std::memory_order_relaxed);
+            ab22.store(true, std::memory_order_relaxed);
+        }
+        // thread2
+        {
+            std::cout << ab22.load(std::memory_order_relaxed) << std::endl;
+            std::cout << ab21.load(std::memory_order_relaxed) << std::endl;
+        }
+        // может быть вывод : 0 0, 0 1, 1 1, 1 0
+
+        std::cout << "\ntest 3" << std::endl;
+        // memory_order_seq_cst(default) гарантирует тот же порядок изменений переменных в другом потоке
+        // значение измененной переменной сразу же будут видны в другом потоке
+        // также гарантирует что значения измененных переменных до, даже если они не атомики тоже будут видны в другом потоке
+        bool b3{false};
+        std::atomic<bool> ab31{false};
+        std::atomic<bool> ab32{false};
+        // thread1
+        {
+            b3 = true;
+            ab31.store(true, std::memory_order_seq_cst);
+            ab32.store(true, std::memory_order_seq_cst);
+        }
+        // thread2
+        {
+            std::cout << ab32.load(std::memory_order_seq_cst) << std::endl;
+            std::cout << ab31.load(std::memory_order_seq_cst) << std::endl;
+            if (ab31.load(std::memory_order_seq_cst))
+                assert(b3);
+        }
+        // может быть вывод : 0 1, 0 0 или 1 1, те если второе значение изменилось то первое 100% тоже изменилось
+
+        std::cout << "\ntest 4" << std::endl;
+        // модификация атомарной переменной c a.store(new_value, memory_order_release) сразуже будет видна в другом потоке
+        // value = a.load(memory_order_acquire)
+        // !!! все модификации памяти в потоке до .store() будут видны в другом потоке посде .load()
+        // но если несколькими тредами записывать разные переменные то эти переменные читающими тердами могут увидится в разном порядке
+        // тоесть нет синхронизации между атомиками если они записываются в разных потоках( с флагом memory_order_seq_cst есть)
+        // пример как возможная реализация мьютекса
+        /*
+        class mutex {
+        public:
+            void lock() {
+                bool expected = false;
+                while (!_locked.compare_exchange_weak(expected, true, std::memory_order_acquire)) {
+                    expected = false;
+                }
+            }
+
+            void unlock() {
+                _locked.store(false, std::memory_order_release);
+            }
+
+        private:
+            std::atomic<bool> _locked;
+        };
+        */
     }
 }
