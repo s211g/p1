5971545d3e1fffa0188088d4d0502028b683bf56
Ser <mail@mail.com>
2022-09-26 13:29:00 +0300

    1

--- Containers/containers_test.cpp
+++ Containers/containers_test.cpp
@@ -6,6 +6,9 @@
 #include <unordered_map>
 #include <algorithm>
 #include <utility>
+#include <tuple>
+
+#include "containers_test.hpp"
 
 namespace containers_test {
 
@@ -82,6 +85,73 @@ namespace containers_test {
         populate_um(1000);
         populate_um(1000);
         populate_um(1000);
+    }
+
+    template <typename T>
+    void fff(T s) {
+        std::cout << s << '\n';
+    }
+
+    template <typename T>
+    void printTupleInfo(T& t) {
+        std::cout << "\ntuple info: " << std::endl;
+        std::cout << "tuple size = " << std::tuple_size_v<std::remove_reference<decltype(t)>::type> << std::endl;
+        //std::cout << "tuple size = " << std::tuple_size_v<decltype(t)> << std::endl;
+        std::cout << "[0] : " << std::get<0>(t) << std::endl;
+        std::cout << "[1] : " << std::get<1>(t) << std::endl;
+        std::cout << "[0] type: " << type2name<std::tuple_element_t<0, std::remove_reference<decltype(t)>::type>>() << std::endl;
+        std::cout << "[1] type: " << type2name<std::tuple_element_t<1, std::remove_reference<decltype(t)>::type>>() << std::endl;
+        std::cout << "[0] type decltype(std::get<0>(t)) : " << type2name<decltype(std::get<0>(t))>() << std::endl;
+        std::cout << "[1] type decltype(std::get<0>(t)) : " << type2name<decltype(std::get<1>(t))>() << std::endl;
+    }
+
+    void test_tuple() {
+        std::cout << "\ntest std::tuple" << std::endl;
+
+        std::cout << "\ntest 1" << std::endl;
+        std::tuple<int, std::string> t = {1, "_1_"};
+        printTupleInfo(t);
+        int i1 = 11;
+        int j1 = 22;
+        // std::tie - создает кортеж из ссылок, параметры ссылки на lvalue
+        auto t1 = std::tie(i1, j1);
+        printTupleInfo(t1);
+
+        std::cout << "\ntest 2" << std::endl;
+        using t_t           = std::tuple<int, std::string>;
+        std::vector<t_t> vt = {{4, "_4_"}, {1, "_1_"}, {2, "_2_"}, {3, "_3_"}, {3, "0_3_"}};
+        auto print_vt       = [](std::vector<t_t>& vt) {
+            using t_t = std::tuple<int, std::string>;
+            std::for_each(vt.begin(), vt.end(), [](t_t& t) { std::cout << std::get<0>(t) << "," << std::get<1>(t) << std::endl; });
+        };
+        std::sort(vt.begin(), vt.end(), [](auto&& t1, auto&& t2) { return t1 < t2; });
+        print_vt(vt);
+
+        std::cout << "\ntest 3 sort by firts,second value" << std::endl;
+        std::sort(vt.begin(), vt.end(), [](auto&& t1, auto&& t2) { return std::tie(std::get<0>(t1), std::get<1>(t1)) < std::tie(std::get<0>(t2), std::get<1>(t2)); });
+        print_vt(vt);
+
+        std::cout << "\ntest 4 sort by second,first value" << std::endl;
+        std::sort(vt.begin(), vt.end(), [](auto&& t1, auto&& t2) { return std::tie(std::get<1>(t1), std::get<0>(t1)) < std::tie(std::get<1>(t2), std::get<0>(t2)); });
+        print_vt(vt);
+
+        std::cout << "\ntest 5 (futures)" << std::endl;
+        auto [i, j, k] = std::tuple{1, 2, 3};
+        auto t5        = std::make_tuple(i, std::ref(j), k);
+        auto t6        = std::tuple{1, std::ref(j), 3};
+        static_assert(std::is_same_v<decltype(t5), std::tuple<int, int&, int>>);
+        static_assert(std::is_same_v<decltype(t6), std::tuple<int, std::reference_wrapper<int>, int>>);
+
+        std::cout << type2name(0) << std::endl;
+        std::cout << type2name(&i) << std::endl;
+        std::cout << type2name(std::ref(i)) << std::endl;
+
+        std::cout << "\ntest 6 std::tuple_cat" << std::endl;
+        auto t61 = std::make_tuple(1, "2");
+        auto t62 = std::make_tuple(3, "4");
+        auto t63 = std::tuple_cat(t61, t62);
+        std::cout << "tuple size = " << std::tuple_size_v<decltype(t63)> << std::endl;
+        std::cout << std::get<0>(t63) << "," << std::get<1>(t63) << "," << std::get<2>(t63) << "," << std::get<3>(t63) << "," << std::endl;
     }
 
 }
--- Containers/containers_test.hpp
+++ Containers/containers_test.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <type_traits>
+#include <functional>
 
 namespace containers_test {
 
@@ -8,12 +11,48 @@ namespace containers_test {
     void test_vector();
     void test_set();
     void test_unordered_map();
+    void test_tuple();
+
+    template <typename T>
+    std::string type2name(T) {
+        if (std::is_same_v<T, int>)
+            return "int";
+        if (std::is_same_v<T, int&>)
+            return "int&";
+        if (std::is_same_v<T, int*>)
+            return "int*";
+        if (std::is_same_v<T, std::reference_wrapper<int>>)
+            return "ref<int>";
+        if (std::is_same_v<T, std::string>)
+            return "string";
+        if (std::is_same_v<T, std::string&>)
+            return "string&";
+        return "?";
+    }
+
+    template <typename T>
+    std::string type2name() {
+        if (std::is_same_v<T, int>)
+            return "int";
+        if (std::is_same_v<T, int&>)
+            return "int&";
+        if (std::is_same_v<T, int*>)
+            return "int*";
+        if (std::is_same_v<T, std::reference_wrapper<int>>)
+            return "ref<int>";
+        if (std::is_same_v<T, std::string>)
+            return "string";
+        if (std::is_same_v<T, std::string&>)
+            return "string&";
+        return "?";
+    }
 
     inline void test() {
         std::cout << "containers tests" << std::endl;
         //test_array();
         //test_vector();
         //test_set();
-        test_unordered_map();
+        //test_unordered_map();
+        test_tuple();
     }
 }
