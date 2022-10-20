1ace33821b403f1d53c208fe0cae11bb2209f6ab
Ser <mail@mail.com>
2022-10-20 15:50:05 +0300

    1

--- Other/initdata_test.cpp
+++ Other/initdata_test.cpp
@@ -1,5 +1,6 @@
 
 #include "initdata_test.hpp"
+#include "TypeUtils.hpp"
 #include <mutex>
 #include <functional>
 
@@ -44,14 +45,19 @@ namespace initdata_test {
     void test_lambda() {
         std::cout << "test lambda" << std::endl;
 
-        // общее:
+        // Общее:
+
         // в замыкание все значения передаются как const копии
         // чтобы переместить не копируемые объекты(напр unique_ptr) надо сделать их инициализацию в лямбда выражении:
         // [var = move(var)]
+
         // !!! при объявлении ламбда выражений в теле функций класса с видимостью [=]
         // члены класса становятся доступны неявно через скрытый this,
         // время сущестования ляьбды в этом случае не должно превышать
         // времени существования объекта этого класса
+
+        // для вывода типа аргументов [](auto&&... arg) в теле лямбды надо использовать decltype():
+        // f(std::forward<decltype(arg)>(arg)...);
 
         std::cout << "\ntest 1 move objects to lambda" << std::endl;
         class A {
@@ -76,18 +82,19 @@ namespace initdata_test {
             }
         };
 
-        A a1(1);
-        std::unique_ptr<A> ua1 = std::make_unique<A>(2);
-        int j                  = 123;
-        auto l                 = [=, ua = std::move(ua1), pa = new A(3)] {
-            std::cout << "j = " << j << std::endl;
-            std::cout << "a1.i = " << a1.i << std::endl;   // объект a1 - копия a1 до объявления ламбды
-            std::cout << "ua->i = " << ua->i << std::endl; // ua - перенесено через инициализирующее объявление
-            std::cout << "pa->i = " << pa->i << std::endl; // создано при объявлении
-        };
+        {
+            A a1(1);
+            std::unique_ptr<A> ua1 = std::make_unique<A>(2);
+            int j                  = 123;
+            auto l                 = [=, ua = std::move(ua1), pa = new A(3)] {
+                std::cout << "j = " << j << std::endl;
+                std::cout << "a1.i = " << a1.i << std::endl;   // объект a1 - копия a1 до объявления ламбды
+                std::cout << "ua->i = " << ua->i << std::endl; // ua - перенесено через инициализирующее объявление
+                std::cout << "pa->i = " << pa->i << std::endl; // создано при объявлении
+            };
 
-        l();
-
+            l();
+        }
         std::cout << "\ntest 2 lambda and class members" << std::endl;
 
         std::function<void()> f2;
@@ -101,6 +108,19 @@ namespace initdata_test {
             f2();
         }
         f2(); // висячая ссылка на член класса внутри лямбды, лямбда ссылается на удаленный объект
+
+        std::cout << "\ntest 3 lambda(auto&&)" << std::endl;
+        auto l3 = [](auto&&... arg) {
+            std::cout << "arg type : " << ((type_utils::type2name<decltype(arg)>() + " ") + ...) << std::endl;
+            {
+                A a(std::forward<decltype(arg)>(arg)...);
+            }
+            std::cout << std::endl;
+        };
+        l3(1); // arg type : int&&, A::A(int) i = 1
+        A a3(3);
+        l3(a3);   // arg type: A&, A::A(const A&) i = 3
+        l3(A{4}); // arg type : A&&, A::A(A&&) i = 4
     }
 
 }
