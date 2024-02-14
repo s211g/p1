40ec7a30c3c86552c66a32f1cc106fdf5649a5fb
s211g <s211529@mail.ru>
2024-02-14 12:44:40 +0300

    1

--- main.cpp
+++ main.cpp
@@ -2,22 +2,37 @@
 #include <memory>
 #include <functional>
 
+
+// Containers
+#include "containers_test.hpp"
+
+// Inheritance
+#include "inheritance_test.hpp"
+
+// Mutex
 #include "mutex_test.hpp"
-#include "smartpointer_test.hpp"
-#include "scopeguard_test.hpp"
 
-// Thread
-#include "thread_test.hpp"
-
-// TypeDeclaration
-#include "typedeclaration_test.hpp"
-
-#include "initdata_test.hpp"
+// Other
+#include "atomic_hdr_test.hpp"
+#include "chrono_hdr_test.hpp"
+#include "constructor_test.hpp"
 #include "functional_hdr_test.hpp"
 #include "future_hdr_test.hpp"
-#include "chrono_hdr_test.hpp"
-#include "atomic_hdr_test.hpp"
-#include "containers_test.hpp"
+#include "initdata_test.hpp"
+#include "rtti_cast_test.hpp"
+#include "rvalue_test.hpp"
+#include "static_test.hpp"
+
+// Patterns
+#include "patterns_creational_test.hpp"
+#include "patterns_structure_test.hpp"
+#include "patterns_behavior_test.hpp"
+
+// ScopeGuard
+#include "scopeguard_test.hpp"
+
+// SmartPointer
+#include "smartpointer_test.hpp"
 
 // Template
 #include "template_explicit_specialization_test.hpp"
@@ -26,54 +41,66 @@
 #include "template_crtp_test.hpp"
 #include "template_variable_arguments_test.hpp"
 
-// Patterns
-#include "patterns_creational_test.hpp"
-#include "patterns_structure_test.hpp"
-#include "patterns_behavior_test.hpp"
+// Thread
+#include "thread_test.hpp"
 
+// TypeDeclaration
+#include "typedeclaration_test.hpp"
+
+// Virtual
 #include "virtual_function_test.hpp"
-#include "rtti_cast_test.hpp"
-#include "rvalue_test.hpp"
-#include "constructor_test.hpp"
-#include "static_test.hpp"
-#include "inheritance_test.hpp"
+
 
 int main() {
     std::cout << "sizeof(void*) : " << sizeof(void*) << std::endl
               << std::endl;
 
-    //constructor_test::test();
-    //rvalue_test::test();
+    // Containers
+    containers_test::test();
 
-    //virtual_function_test::test();
+    // Inheritance
+    // inheritance_test::test();
 
-    //containers_test::test();
-    //mutex_test::test();
-    //smartpointer_test::test();
-    //scopeguard_test::test();
-    //thread_test::test();
-    //vatemplate_test::test();
-    //typedeclaration_test::test();
-    //initdata_test::test();
-    //functional_hdr_test::test();
-    //future_hdr_test::test();
-    //chrono_hdr_test::test();
-    //atomic_hdr_test::test();
+    // Mutex
+    // mutex_test::test();
 
-    //template_explicit_specialization_test::test();
-    //template_template_specialization_test::test();
-    //template_test::test();
-    //template_crtp_test::test();
-    template_variable_arguments_test::test();
+    // Other
+    // atomic_hdr_test::test();
+    // chrono_hdr_test::test();
+    // constructor_test::test();
+    // functional_hdr_test::test();
+    // future_hdr_test::test();
+    // initdata_test::test();
+    // rtti_cast_test::test();
+    // rvalue_test::test();
+    // static_test::test();
 
-    //patterns_creational_test::test();
-    //patterns_structure_test::test();
-    //patterns_behavior_test::test();
+    // Patterns
+    // patterns_creational_test::test();
+    // patterns_structure_test::test();
+    // patterns_behavior_test::test();
 
-    //rtti_cast_test::test();
-    //static_test::test();
+    // ScopeGuard
+    // scopeguard_test::test();
 
-    //inheritance_test::test();
+    // SmartPointer
+    // smartpointer_test::test();
+
+    // Template
+    // template_explicit_specialization_test::test();
+    // template_template_specialization_test::test();
+    // template_test::test();
+    // template_crtp_test::test();
+    // template_variable_arguments_test::test();
+
+    // Thread
+    // thread_test::test();
+
+    // TypeDeclaration
+    // typedeclaration_test::test();
+
+    // Virtual
+    // virtual_function_test::test();
 
     return 0;
 }
